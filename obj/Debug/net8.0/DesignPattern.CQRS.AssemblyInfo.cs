a34be06785dc80da88390eee972f37a546efeeac
Acaringen <alkimacaracarel@gmail.com>
2025-04-26 18:06:22 +0300

    güncelleme

--- obj/Debug/net8.0/DesignPattern.CQRS.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesignPattern.CQRS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPattern.CQRS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a54ca9911ae01006b0f08fbb4d5f03b9e9f39b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPattern.CQRS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPattern.CQRS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
